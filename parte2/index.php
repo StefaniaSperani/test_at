f41b81e049bf97583d03f51bb0e62f6682e6b01f
StefaniaSperani <thedarkcry@hotmail.it>
2023-06-05 15:40:11 +0200

    fix

--- parte2/index.php
+++ parte2/index.php
@@ -3,16 +3,13 @@ use controllers\HomeController;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Factory\AppFactory;
-use Slim\Views\Twig;
-use Slim\Views\TwigMiddleware;
-use Slim\Views\TwigExtension;
+
 
 require __DIR__ . '/vendor/autoload.php';
 
 
 $app = AppFactory::create();
 $app->setBasePath("/test_at/parte2");
-
 
 
 $app->addRoutingMiddleware();
