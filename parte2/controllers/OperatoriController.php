fdf60a36ff693e2e9228ae1b3c89ecde4b731007
StefaniaSperani <thedarkcry@hotmail.it>
2023-06-08 10:52:00 +0200

    edit controllers

--- parte2/controllers/OperatoriController.php
+++ parte2/controllers/OperatoriController.php
@@ -53,7 +53,6 @@ class OperatoriController
             $username = $data["username"];
             $password = $data["password"];
             $stato = $data["stato"];
-
             $statoInt = $stato === 'true' ? 'Attivo' : 'Non attivo';
 
             $sql = "INSERT INTO operators (nome, cognome, mansione, username, password, stato) VALUES ('$nome', '$cognome', '$mansione', '$username', '$password', '$statoInt')";
@@ -70,6 +69,7 @@ class OperatoriController
             return $response
                 ->withStatus(302)
                 ->withHeader("Location", $urlOperatori);
+
         }
     }
 
@@ -78,14 +78,67 @@ class OperatoriController
     {
 
         $view = Twig::fromRequest($request);
-        return $view->render($response, 'operatori/edit.html', []);
 
+        if (isset($args['id'])) {
+            $conn = getDbConn();
+            $id = $args['id'];
+            $sql = "SELECT * FROM operators WHERE id='$id'";
+            $result = mysqli_query($conn, $sql);
+            $user = mysqli_fetch_assoc($result);
+
+            return $view->render($response, 'operatori/edit.html', ['user' => $user]);
+        } else {
+            $response->getBody()->write("Errore nella query");
+            return $response->withStatus(500);
+        }
+
+        // $view = Twig::fromRequest($request);
+        // return $view->render($response, 'operatori/edit.html', []);
+
+    }
+
+    // Funzione che serve per la modifica dell'operatore
+    public function editSubmit(ServerRequestInterface $request, ResponseInterface $response, array $args)
+    {
+        if ($request->getMethod() === 'POST' && $_POST['submit']) {
+            $conn = getDbConn();
+
+            $id = $args['id'];
+            $nome = $request->getParsedBody()['nome'];
+            $cognome = $request->getParsedBody()['cognome'];
+            $mansione = $request->getParsedBody()['mansione'];
+            $stato = $request->getParsedBody()['stato'];
+            $statoInt = $stato === 'true' ? 'Attivo' : 'Non attivo';
+
+            $sql = "UPDATE operators SET nome='$nome', cognome='$cognome', mansione='$mansione', stato='$statoInt' WHERE id='$id'";
+
+            $result = mysqli_query($conn, $sql);
+
+            if ($result) {
+                $response->getBody()->write('<div class="text-center text-danger fs-3 mt-3"> Operatore modificato con successo! </div>');
+            } else {
+                $response->getBody()->write('<div class="text-center text-danger fs-3 mt-3"> Errore durante la modifica dell\'operatore: </div>' . $conn->error);
+            }
+
+            // Premuto il submit vengono reindirizzati alla pagina principale(la lista degli operatori)
+            $routeParser = RouteContext::fromRequest($request)->getRouteParser();
+            $urlOperatori = $routeParser->urlFor("operatori.index");
+            return $response
+                ->withStatus(302)
+                ->withHeader("Location", $urlOperatori);
+
+        }
     }
 }
 
 
+// ROTTE
 
+// Pagina principale con la lista degli operatori
 $app->get("/operatori", [OperatoriController::class, "operators"])->setName("operatori.index");
+// Pagina di aggiunta operatore
 $app->get("/operatori/add", [OperatoriController::class, "add"]);
 $app->post("/operatori/add", [OperatoriController::class, "submit"]);
+// Pagina di modifica operatore
 $app->get("/operatori/edit", [OperatoriController::class, "edit"]);
+$app->post("/operatori/edit", [OperatoriController::class, "editSubmit"]);
